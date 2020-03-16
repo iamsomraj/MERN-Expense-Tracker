c1add89701b3d8a78ce0c843e8ee67925f15289a
iamsomraj <iamsomraj@gmail.com>
2020-03-16 21:57:42 +0530

    Updates

---
+++ precache-manifest.cc9573c03cb2967a51504b9f0a1a5e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a83381c6b13b6037498843c9bfc408",
+    "url": "/MERN-Expense-Tracker/index.html"
+  },
+  {
+    "revision": "de081ceed375410c8322",
+    "url": "/MERN-Expense-Tracker/static/css/main.2f7121da.chunk.css"
+  },
+  {
+    "revision": "aaea40ab0f54d09ec877",
+    "url": "/MERN-Expense-Tracker/static/js/2.bf88a971.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MERN-Expense-Tracker/static/js/2.bf88a971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de081ceed375410c8322",
+    "url": "/MERN-Expense-Tracker/static/js/main.10425bf5.chunk.js"
+  },
+  {
+    "revision": "6378bded2f0e35f4959c",
+    "url": "/MERN-Expense-Tracker/static/js/runtime-main.c5aad7e8.js"
+  }
+]);
